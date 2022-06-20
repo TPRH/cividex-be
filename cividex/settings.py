226a1796eb1fc56b02a2586de7d7bf7578515b36
lg550055 <lg550055@hotmail.com>
2022-06-20 12:21:19 -0500

    Adds heroku to allowed hosts

--- cividex/settings.py
+++ cividex/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','cividex.herokuapp.com']
 
 # Application definition
 INSTALLED_APPS = [
